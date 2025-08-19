85fb16d55b2034b639374e1481eac00af4e31325
sahil-wadhwa <sahil06.wadhwa@gmail.com>
2025-08-19 11:48:55 +0530

    Update ab.js

--- models/ab.js
+++ models/ab.js
@@ -3,9 +3,15 @@ const mongoose = require("mongoose");
 const registrationSchema = new mongoose.Schema(
   {
     fullName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: {
+  type: String,
+  match: [/\S+@\S+\.\S+/, "Invalid email format"],
+},
     university: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
+    phoneNumber: {
+  type: String,
+  match: [/^\d{10}$/, "Invalid phone number"],
+},
     delegateType: { type: String, required: true },
     committeePreference1: { type: String },
     committeePreference2: { type: String },
